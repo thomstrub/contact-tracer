88c5618d7146d62491f31c788dfcc4f8ed8643a9
Thomas Strub <Thom@Thomass-Air.hitronhub.home>
2020-11-01 11:52:54 -0800

    edited class for tiles with functions

--- js/main.js
+++ js/main.js
@@ -6,6 +6,15 @@ const difficulty = {
     medium: .1563
 };
 
+// each tile should have an id
+// each tile needs a variable to store adjoining tiles
+// each tile needs a variable to store if it has been revealed or not
+// each tile needs a variable to store whether it is a virus or not
+// each tile needs a variable to store whether it has been quarentined or not
+// the user should be able to toggle between the revealed state
+// if no adjacent tiles are viruses, the reveal function needs to spread to the 
+// adjacent tiles. This should continue recursively.
+
 class Tile {
     constructor(id, adjoiningTiles, isReveald, isVirus, isQuarentined) {
         this.id = id;
@@ -14,7 +23,36 @@ class Tile {
         this.isVirus = isVirus;
         this.isQuarentined = isQuarentined;
     }
+    toggleRevealed() {
+        this.isReveald = true;
+    }
+
+    // tally the number of adjacent squares in which 'isVirus = true'
+    countAdjacentlVirus() {
+        let count = 0;
+        this.adjoiningTiles.forEach(function(adjoined){
+            if (adjoined.isVirus === true) {
+                count++;
+            }
+            return count;
+        })
+        return
+    }
+
+    // when a tile is revealed by the user, this.revealed = true
+    // if it has no adjacent virus, each adjacent virus is revealed and so on.
+    revealAndSpread() {
+        this.toggleRevealed();
+        if(this.countAdjacentlVirus() > 0){
+            return;
+        } else {
+            this.adjoiningTiles.forEach(function(adjoined){
+                adjoined.spread();
+            });
+        }
+    }
 }
+
 
 
 /*----- app's state (variables) -----*/
@@ -30,24 +68,31 @@ let board = [
     [null,null,null,null,null,null,null,null,null]
     ];
 
-
-
+    let timer;
+    let virusCount;
+    let setting;
 /*----- cached element references -----*/
 let gameBoard = document.getElementById('board')
 
 /*----- event listeners -----*/
 
+//
+
 
 
 /*----- functions -----*/
 function initialize(){
+    timer = 0;
+    setting = difficulty.easy;
+    virusCount = Math.floor(rows * columns * setting);
 
 }
 
 function initializeBoard(){
     let board = [];
     let row = [];
-    let counter = 0;
+    let counter = 0; // don't really need the counter- the id of the tiles is a counter- maybe use modulus w/ 9?
+    let virusLocations = [];
     for(let i = 0; i < rows; i++){
         for(let j = 0; j < columns; j++){
             counter++
@@ -63,8 +108,8 @@ function initializeBoard(){
 }
 initializeBoard();
 
-function virusInit(num){
-    // Math.floor the difficulty factor multiplied by the number of tiles to determine virus number
+
+function virusInit(counter, virus){
     // for each virus, / use Math.flooor(Math.random) to pick ten numbers between 1 and 81, then use
                 // a counter to determine if that tile is a virus or not
 }
